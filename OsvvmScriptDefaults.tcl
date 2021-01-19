64959b6ba31b652e436ddf2c886b9365b2d99d1a
Jim Lewis <jim@synthworks.com>
2021-01-18 22:00:06 -0800

    Refactored tool settings from StartUp.tcl

---
+++ OsvvmScriptDefaults.tcl
@@ -0,0 +1,54 @@
+#  File Name:         OsvvmScriptDefaults.tcl
+#  Purpose:           Scripts for running simulations
+#  Revision:          OSVVM MODELS STANDARD VERSION
+# 
+#  Maintainer:        Jim Lewis      email:  jim@synthworks.com 
+#  Contributor(s):            
+#     Jim Lewis      email:  jim@synthworks.com   
+# 
+#  Description
+#    Sets the defaults for the OSVVM Scripts
+#    
+#  Developed by: 
+#        SynthWorks Design Inc. 
+#        VHDL Training Classes
+#        OSVVM Methodology and Model Library
+#        11898 SW 128th Ave.  Tigard, Or  97223
+#        http://www.SynthWorks.com
+# 
+#  Revision History:
+#    Date      Version    Description
+#     2/2021   2021.02    Refactored Default Settings from StartUp.tcl
+#
+#
+#  This file is part of OSVVM.
+#  
+#  Copyright (c) 2021 by SynthWorks Design Inc.  
+#  
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  
+#      https://www.apache.org/licenses/LICENSE-2.0
+#  
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+# VHDL Simulation time units - Simulator is started with this value
+set SIMULATE_TIME_UNITS        ps
+
+# Start out with library location unset
+  if {[info exists LIB_BASE_DIR]} {
+    unset LIB_BASE_DIR 
+  }
+# Only Set library location if it is different from the simulation directory
+# set LIB_BASE_DIR C:/tools/sim_temp
+
+# Set time units if project settings file did not
+if {![info exists SIMULATE_TIME_UNITS]} {
+  set SIMULATE_TIME_UNITS ps
+}
